a4a6bbef293b43dfb759a30a3d91260d2d07df4c
VakoNarimanidze <vako.narimanidze5@gmail.com>
2023-01-19 15:22:58 +0400

    1-3  homework added

---
+++ if-else.js
@@ -0,0 +1,6 @@
+let age = 17
+if (age >= 18){
+    console.log("you are an adult");
+}else if (age < 18){
+    console.log("you are a teenager");
+}
---
+++ script.js
@@ -0,0 +1,10 @@
+let person = 
+    {
+        name : "Giorgi" ,
+        lastname : "Macharidze",
+        age : 20,
+        address : "Tbilisi"
+    }
+console.log(`My name is ${person.name} ${person.lastname}`);
+console.log(`I am ${person.age} years old`);
+console.log(`I live in ${person.address}`);
