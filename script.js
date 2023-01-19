45c299b926c95f1c127a5dedb5f467cc3f605d9e
VakoNarimanidze <vako.narimanidze5@gmail.com>
2023-01-19 22:05:40 +0400

    .

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 let person = 
     {
         name : "Giorgi" ,
-        lastname : "Macharidze",
+        lastName : "Macharidze",
         age : 20,
         address : "Tbilisi"
     }
-console.log(`My name is ${person.name} ${person.lastname}`);
+console.log(`My name is ${person.name} ${person.lastName}`);
 console.log(`I am ${person.age} years old`);
 console.log(`I live in ${person.address}`);
